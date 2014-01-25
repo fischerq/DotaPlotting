5beebe9a209615551119b73ccbbb89efae11807d
Zera <lord_zera@gmx.de>
2014-01-25 18:24:14 +0100

    first plot

--- plot.py
+++ plot.py
@@ -2,9 +2,26 @@ from utils import *
 
 import samples
 
-filename = "data/homework02.csv"
-types = [float, float, float,]
+filename = "data/position_data_raw.csv"
+types = [int, int, float, float]
 features = ["PlayerID", "GameTime", "X", "Y"]
 
 position_format = samples.SampleFormat(features, types)
-samples_regression = parse_csv(filename, regression_format)
+samples_positions = parse_csv(filename, position_format)
+
+from matplotlib import pyplot as PLT
+from matplotlib import cm as CM
+
+gridsize=50
+PLT.subplot(111)
+
+# if 'bins=None', then color of each hexagon corresponds directly to its count
+# 'C' is optional--it maps values to x-y coordinates; if 'C' is None (default) then
+# the result is a pure 2D histogram
+
+PLT.hexbin(samples_positions["X"], samples_positions["Y"], C=None, gridsize=gridsize, cmap=CM.jet, bins=None)
+PLT.axis([-8576, 9216.0, -7680.0, 8192.0])
+
+cb = PLT.colorbar()
+cb.set_label('mean value')
+PLT.show()
--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import csv
 from samples import SampleFormat
 
+COLUMNS = 0
+ROWS = 1
 
-def parse_csv(filename, sample_format):
-    result = []
+
+def parse_csv(filename, sample_format, parse_type=COLUMNS):
+    result = {}
     with open(filename, 'rb') as csv_file:
         reader = csv.reader(csv_file)
         header = reader.next()
@@ -12,6 +15,14 @@ def parse_csv(filename, sample_format):
             if header[i] not in sample_format.features:
                 print "row not specified as feature: "+header[i]
 
-        for row in reader:
-            result.append(sample_format.parse(row))
+        if parse_type is ROWS:
+            for row in reader:
+                result.append(sample_format.parse(row))
+        else:
+            for feature in sample_format.features:
+                result[feature]=[]
+            for row in reader:
+                row = sample_format.parse(row)
+                for feature in sample_format.features:
+                    result[feature].append(row.feature(feature))
         return result
